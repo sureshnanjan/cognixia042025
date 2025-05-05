d088a3a4f14c22ea600bd0555e631eed89f22829
Vaishnavi Chennapragada <sreelakshmi.c@ascendion.com>
2025-05-05 13:03:27 +0530

    add tasks

--- src/python/get_started.py
+++ src/python/get_started.py
@@ -5,11 +5,11 @@ command_executor="http://localhost:8090"
 chrome_options = ChromeOptions()
 #driver1 = webdriver.Chrome()
 #driver2 = webdriver.Firefox()
-#driver1.get('https://the-internet.herokuapp.com/')
 driver = webdriver.Remote(
         command_executor=command_executor,
         options=chrome_options
     )
+#driver1.get('https://the-internet.herokuapp.com/')
 driver.get('https://the-internet.herokuapp.com/')
 assert 'The Internet' in driver.title
 #assert 'The Internet' in driver2.title
