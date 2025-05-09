e24517d69e8c08ea6fc3b44e92c376860b7ad4e8
Ganesh Korasala <ganesh.korasala@ascendion.com>
2025-05-09 16:42:12 +0530

    Trainigs

--- src/python/get_started.py
+++ src/python/get_started.py
@@ -1,19 +1,37 @@
 from selenium import webdriver
+
 from selenium.webdriver.common.by import By
+
 from selenium.webdriver.chrome.options import Options as ChromeOptions
+
 command_executor="http://localhost:8090"
+
+#command_executor="http://localhost:4444/wd/hub"
+
 chrome_options = ChromeOptions()
-#driver1 = webdriver.Chrome()
-#driver2 = webdriver.Firefox()
-#driver1.get('https://the-internet.herokuapp.com/')
-driver = webdriver.Remote(
+
+driver1 = webdriver.Chrome()
+ 
+driver1 = webdriver.Remote(
         command_executor=command_executor,
         options=chrome_options
-    )
-driver.get('https://the-internet.herokuapp.com/')
-assert 'The Internet' in driver.title
+   )
+ 
+#driver2 = webdriver.Firefox()
+
+driver1.get('https://the-internet.herokuapp.com/')
+ 
+driver1.get('https://the-internet.herokuapp.com/')
+
+assert 'The Internet' in driver1.title
+
 #assert 'The Internet' in driver2.title
+
 #elem = driver.find_element(By.ID, 'm-documentationwebdriver')
+
 #elem.click()
+
 #assert 'WebDriver' in driver.title
+
 #driver.quit()
+ 
