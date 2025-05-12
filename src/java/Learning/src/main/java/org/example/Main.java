46a9e4e3592f3c082ceedbb8a5b0e1e4e97b9c42
Vaishnavi sivakumar <vaishnavi.sivakumar@ascendion.com>
2025-05-12 16:39:37 +0530

    new files

--- src/java/Learning/src/main/java/org/example/Main.java
+++ src/java/Learning/src/main/java/org/example/Main.java
@@ -1,9 +1,7 @@
 package org.example;
-<<<<<<< HEAD
 import examples.ClassMemberDemo;
 import java.util.*;
 
-=======
 import examples.*;
 
 import java.util.Arrays;
@@ -12,21 +10,21 @@ import java.util.function.Function;
 import java.util.function.IntPredicate;
 
 import static java.lang.String.format;
->>>>>>> 29e55bc4b4d705ec31dffba628a9c9fa6fcc2a38
+
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    int text;
+
     //float text;
-    BinaryOperator<Integer> c = (param1, param2)  -> param1 + param2;
+    BinaryOperator<Integer> c = (param1, param2) -> param1 + param2;
+
     public static void main(String[] args) {
         //add("one", "two");
         //anyMatchDemo();
-        //LangTranslator();
-        int[] numbers = {10,20,11,2,6,9,12,19,100};
-        System.out.println(Arrays.stream(numbers).collect(()-> 100,(a,b)-> System.out.println("a +b"),(a,b)-> System.out.println("a * b")));
-
+        LangTranslator();
+        int[] numbers = {10, 20, 11, 2, 6, 9, 12, 19, 100};
+        System.out.println(Arrays.stream(numbers).collect(() -> 100, (a, b) -> System.out.println("a +b"), (a, b) -> System.out.println("a * b")));
 
 
     }
@@ -35,7 +33,7 @@ public class Main {
         IntPredicate even_checker = new IntPredicate() {
             @Override
             public boolean test(int value) {
-                return value /2 == 0;
+                return value / 2 == 0;
             }
         };
         IntPredicate morethan100checker = new IntPredicate() {
@@ -45,17 +43,17 @@ public class Main {
             }
         };
 
-        class EvenChecker implements IntPredicate{
+        class EvenChecker implements IntPredicate {
             @Override
             public boolean test(int value) {
-                return value /2 == 0;
+                return value / 2 == 0;
             }
         }
 
-        int[] numbers = {1,10,8,9,4,3,2,0};
-        System.out.println(Arrays.stream(numbers).anyMatch(c-> c / 2 ==0));
+        int[] numbers = {1, 10, 8, 9, 4, 3, 2, 0};
+        System.out.println(Arrays.stream(numbers).anyMatch(c -> c / 2 == 0));
         // Any number greater than 100
-        System.out.println(Arrays.stream(numbers).anyMatch(c-> c > 100));
+        System.out.println(Arrays.stream(numbers).anyMatch(c -> c > 100));
         System.out.println(Arrays.stream(numbers).anyMatch(even_checker));
         // Any number greater than 100
         System.out.println(Arrays.stream(numbers).anyMatch(morethan100checker));
@@ -64,24 +62,65 @@ public class Main {
 
     private static void LangTranslator() {
         // Methods
-        //functInterface();
-        Translate tamil = c -> {switch (c){
-            case 1:
-                return "Onnu";
-            case 2:
-                return  "Rendu";
-            default:
-                return "Vera number";
-        }};
+        functInterface();
+        Translate tamil = c -> {
+            switch (c) {
+                case 1:
+                    return "Onnu";
+                case 2:
+                    return "Rendu";
+                case 3:
+                    return "Moonu";
+                case 4:
+                    return "Naalu";
+                case 5:
+                    return "Anju";
+                case 6:
+                    return "Aaru";
+                case 7:
+                    return "Yelu";
+                case 8:
+                    return "Yettu";
+                case 9:
+                    return "Onbadhu";
+                default:
+                    return "Vera number";
 
-        Translate telugu = c -> {switch (c){
-            case 1:
-                return "ఒకటి";
-            case 2:
-                return  "రెండు";
-            default:
-                return "Tamil Number";
-        }};
+            }
+        };
+
+        Translate telugu = c -> {
+            switch (c) {
+                case 1:
+                    return "ఒకటి";
+                case 2:
+                    return "రెండు";
+                case 3: return "మూడు";
+                case 4: return "నాలుగు";
+                case 5: return "ఐదు";
+                case 6: return "ఆరు";
+                case 7: return "ఏడు";
+                case 8: return "ఎనిమిది";
+                case 9: return "తొమ్మిది";
+                default: return "తెలియదు";
+
+            }
+        };
+
+        Translate hindi = c -> {
+            switch (c) {
+                case 1: return "एक";
+                case 2: return "दो";
+                case 3: return "तीन";
+                case 4: return "चार";
+                case 5: return "पाँच";
+                case 6: return "छह";
+                case 7: return "सात";
+                case 8: return "आठ";
+                case 9: return "नौ";
+                default: return "अनजान संख्या";
+            }
+        };
 
         // TO DO Implement a Hindi Translator
 
@@ -92,29 +131,58 @@ public class Main {
         * R - the type of the result of the function
         * */
 
-        Function<Integer,String> ftamil = input -> {switch(input){
-            case 1:
-                return "ஒன்று";
-            case 2:
-                return "இரண்டு";
-            default:
-                return "வேறு ஏதாவது";
-        }};
+        Function<Integer, String> ftamil = input -> {
+            switch (input) {
+                case 1:
+                    return "ஒன்று";
+                case 2:
+                    return "இரண்டு";
+                default:
+                    return "வேறு ஏதாவது";
+            }
+        };
 
         ftamil.apply(1);
+
 
         System.out.println(telugu.translate(1));
         System.out.println(telugu.translate(2));
         System.out.println(telugu.translate(3));
+        System.out.println(telugu.translate(4));
+        System.out.println(telugu.translate(5));
+        System.out.println(telugu.translate(6));
+        System.out.println(telugu.translate(7));
+        System.out.println(telugu.translate(8));
+        System.out.println(telugu.translate(9));
+        System.out.println(telugu.translate(10));
         System.out.println(tamil.translate(1));
         System.out.println(tamil.translate(2));
         System.out.println(tamil.translate(3));
+        System.out.println(tamil.translate(4));
+        System.out.println(tamil.translate(5));
+        System.out.println(tamil.translate(6));
+        System.out.println(tamil.translate(7));
+        System.out.println(tamil.translate(8));
+        System.out.println(tamil.translate(9));
+        System.out.println(tamil.translate(10));
+        System.out.println(hindi.translate(1));
+        System.out.println(hindi.translate(2));
+        System.out.println(hindi.translate(3));
+        System.out.println(hindi.translate(4));
+        System.out.println(hindi.translate(5));
+        System.out.println(hindi.translate(6));
+        System.out.println(hindi.translate(7));
+        System.out.println(hindi.translate(8));
+        System.out.println(hindi.translate(9));
+        System.out.println(hindi.translate(10));
+
     }
 
     private static void functInterface() {
-        Calculate add2 = p-> p+2; // lambda expression
-        Calculate double_this = d-> d * 2;
+        Calculate add2 = p -> p + 2; // lambda expression
+        Calculate double_this = d -> d * 2;
         Calculate triple_this = t -> t * 3;
+        Calculate square = x -> x * x;
         //TODO: Implement a sqare/cube function
 
 
@@ -130,24 +198,29 @@ public class Main {
         System.out.println(add2.calculate(40));
         System.out.println(add2.calculate(30));
         System.out.println(add2.calculate(20));
+        System.out.println("Square ");
+        System.out.println(square.calculate(4));
+        System.out.println(square.calculate(5));
+        System.out.println(square.calculate(6));
     }
 
 
     // Signature Name and Number and type of params
-    public static void  CretePerson(){
+    public static void CretePerson() {
         System.out.println("No Params");
     }
-    public static void  CretePerson(String param1){
+
+    public static void CretePerson(String param1) {
         System.out.println("I Have One String params" + param1);
     }
-    public static void  CretePerson(int param1){
+
+    public static void CretePerson(int param1) {
         System.out.println("I Have One Integer params" + param1);
     }
-    public static void  CretePerson(String param1, String param2){
-        System.out.println("I Have Two String params" + param1 + " : "+ param2 );
+
+    public static void CretePerson(String param1, String param2) {
+        System.out.println("I Have Two String params" + param1 + " : " + param2);
     }
-
-
 
 
     private static void literasl_demo() {
@@ -168,37 +241,37 @@ public class Main {
 
         // "When 2 and 2 is added the resulyt will be 4"
         String inter = format(
-                "%s : %s", capA,capAHex);
+                "%s : %s", capA, capAHex);
         int i = 0;
         String mymessage =
-                String.valueOf( new StringBuilder("When")
+                String.valueOf(new StringBuilder("When")
                         .append(i)
                         .append("and")
-                .append(i+2)
-                .append("are added the result is ")
-                .append(10));
+                        .append(i + 2)
+                        .append("are added the result is ")
+                        .append(10));
 
         for (int j = 0; i < 5; i++) {
-            System.out.println(format("When %d and %d is added the resulyt will be %d",i,i+2,add(i,i+2)));
+            System.out.println(format("When %d and %d is added the resulyt will be %d", i, i + 2, add(i, i + 2)));
         }
 
         for (int x = 0; i < 5; i++) {
             System.out.println(new StringBuilder("When")
                     .append(i)
                     .append("and")
-                    .append(i+2)
+                    .append(i + 2)
                     .append("are added the result is ")
-                    .append(add(i,i+2)));
+                    .append(add(i, i + 2)));
 
         }
 
         // Class , Interface , Enum
-        SimpleClass cls = new SimpleClass(0,"");
+        SimpleClass cls = new SimpleClass(0, "");
         Eatable einst = cls;
         Days day = Days.monday;
     }
 
-    private static int add(int a , int b){
+    private static int add(int a, int b) {
         return a + b;
     }
 
@@ -215,15 +288,14 @@ public class Main {
         ClassMemberDemo inst3 = new ClassMemberDemo(3);
         inst3.MethodOne();
         System.out.println("Number of Instances " + ClassMemberDemo.GetInstanceCount());
-<<<<<<< HEAD
-
-
-        }
-
-=======
->>>>>>> 29e55bc4b4d705ec31dffba628a9c9fa6fcc2a38
     }
-}
+
+    }
+
+
+
+
+
 
 
 
