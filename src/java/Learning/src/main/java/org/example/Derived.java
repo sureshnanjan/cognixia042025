19fdc0f1e439d8ed9532bfc6afbd6fee8cc6ef12
sureshnanjan <sureshnanjan@outlook.com>
2025-05-12 16:03:48 +0530

    Generics and Inheritance

---
+++ src/java/Learning/src/main/java/org/example/Derived.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import examples.SimpleClass;
+
+public class Derived extends SimpleClass {
+    public Derived(int number, String name) {
+        super(number, name);
+    }
+
+    public void Test(){
+        this.
+    }
+}
