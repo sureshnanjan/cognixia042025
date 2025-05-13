ba4749570f31e40884c5081c4be310425ff150ab
sureshnanjan <sureshnanjan@outlook.com>
2025-05-13 14:43:43 +0530

    Unit Testing and TDD

--- src/java/Learning/src/main/java/org/example/Main.java
+++ src/java/Learning/src/main/java/org/example/Main.java
@@ -29,6 +29,8 @@ public class Main {
        Bike bike = new Bike();
        Vehicle[] myvehickles = {new Bike(), new Car(), new Bike(), new Car()};
 
+       GenericPet<SimpleClass> mygen = new GenericPet<>();
+
        // Driver - ChromeDriver , FirefoxDriver
         // Vehicle myveh = new Vehicle();
         // Animal -  Abstract concept
