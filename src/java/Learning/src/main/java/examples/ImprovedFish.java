d9c54157031ec53aeda3e66750ed366e491e6dba
Mathumithaa Pugalendhi <mathumithaa.p@ascendion.com>
2025-05-14 11:39:56 +0530

    Pushed latest

---
+++ src/java/Learning/src/main/java/examples/ImprovedFish.java
@@ -0,0 +1,18 @@
+package examples;
+
+public class ImprovedFish implements Moveable,Turnable,Stoppable{
+    @Override
+    public void Move() {
+
+    }
+
+    @Override
+    public void Stop() {
+
+    }
+
+    @Override
+    public void Turn() {
+
+    }
+}
