e7a68c8ddb8f344623fc11853f84c1309bb346c7
Varunkarri21 <137862127+Varunkarri21@users.noreply.github.com>
2025-05-14 16:00:31 +0530

    14thmay

--- src/test/java/herokuapp/tests/HomePageTests.java
+++ src/test/java/herokuapp/tests/HomePageTests.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HomePageTests {
@@ -11,7 +14,7 @@ public class HomePageTests {
     void HomepageTitleisOK(){
         // Arrange
         // Launch HerokuApp
-        String expected = "Welcome to the-internet1";
+        String expected = "Welcome to the-internet";
         ChromeDriver mybrowser = new ChromeDriver();
         mybrowser.get("https://the-internet.herokuapp.com/");
         // Act
@@ -24,8 +27,31 @@ public class HomePageTests {
     }
 
     @Test
-    void HomepageSubTitleisOK(){}
+    void HomepageSubTitleisOK(){
+        String expected = "Available Examples";
+        ChromeDriver mybrowser = new ChromeDriver();
+        mybrowser.get("https://the-internet.herokuapp.com/");
+        // Act
+        //mybrowser.findElement(By.className("heading"));
+        WebElement heading_element = mybrowser.findElement(By.tagName("h2"));
+        String actual = heading_element.getText();
+        // Assert
+        assertEquals(expected, actual, "Heroku sub-Title did not Match");
+    }
 
     @Test
-    void HomepageHasCorrectNumberOfExamples(){}
+    void HomepageHasCorrectNumberOfExamples(){
+        int expected = 44; // Update if needed
+
+        ChromeDriver mybrowser = new ChromeDriver();
+        mybrowser.get("https://the-internet.herokuapp.com/");
+
+        // Use findElements, not findElement
+        List<WebElement> Links = mybrowser.findElements(By.tagName("li"));
+        int actual = Links.size();
+
+        assertEquals(expected, actual, "Number of example links on homepage did not match");
+
+        mybrowser.quit();
+    }
 }
