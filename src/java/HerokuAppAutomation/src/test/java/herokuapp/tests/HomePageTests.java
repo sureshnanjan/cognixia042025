c819d713fdba513e9f7d9e87dc7abd4a5493d584
Athesh Alagarsamy <athesh.alagarsamy@ascendion.com>
2025-05-14 14:37:02 +0530

    test

--- src/java/HerokuAppAutomation/src/test/java/herokuapp/tests/HomePageTests.java
+++ src/java/HerokuAppAutomation/src/test/java/herokuapp/tests/HomePageTests.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HomePageTests {
@@ -11,7 +13,7 @@ public class HomePageTests {
     void HomepageTitleisOK(){
         // Arrange
         // Launch HerokuApp
-        String expected = "Welcome to the-internet1";
+        String expected = "Welcome to the-internet";
         ChromeDriver mybrowser = new ChromeDriver();
         mybrowser.get("https://the-internet.herokuapp.com/");
         // Act
@@ -24,8 +26,29 @@ public class HomePageTests {
     }
 
     @Test
-    void HomepageSubTitleisOK(){}
+    void HomepageSubTitleisOK(){
+        // Arrange
+        String expected = "Available Examples";
+        ChromeDriver mybrowser = new ChromeDriver();
+        mybrowser.get("https://the-internet.herokuapp.com/");
+        // Act
+        WebElement subheading_element = mybrowser.findElement(By.tagName("h2"));
+        String actual = subheading_element.getText();
+        // Assert
+        assertEquals(expected, actual, "Heroku SubTitle did not Match");
+
+    }
 
     @Test
-    void HomepageHasCorrectNumberOfExamples(){}
+    void HomepageHasCorrectNumberOfExamples(){
+        // Arrange
+        int expected = 44;
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        // Act
+        List<WebElement> links = driver.findElements(By.tagName("li"));
+        int actual = links.size();
+        // Assert
+        assertEquals(expected, actual, "Heroku Links count did not Match");
+    }
 }
