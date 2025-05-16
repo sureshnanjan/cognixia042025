a21dc23fa267025f3cd94496921118ec2a976827
sureshnanjan <sureshnanjan@outlook.com>
2025-05-16 17:07:53 +0530

    Test Case with Additional Scenarios

---
+++ src/java/HerokuAppAutomation/src/main/java/heroku/operations/ContextClickOperations.java
@@ -0,0 +1,16 @@
+package heroku.operations;
+
+public interface ContextClickOperations {
+
+    String getTitle();
+
+    String getParagraph(int pos);
+
+    void doHotSpotInvoke();
+
+    String getResultTitle();
+
+    String getResultText();
+
+
+}
